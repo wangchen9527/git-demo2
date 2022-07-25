c4574fb82005cf4a5b1f14ca8255383bdf37f11b
chen <404774609@qq.com>
2022-07-25 11:10:59 +0800

    push test commit

--- src/main/java/com/atguigu/git/test/TestGit.java
+++ src/main/java/com/atguigu/git/test/TestGit.java
@@ -10,5 +10,6 @@ public class TestGit {
         System.out.println("hot-fix!");
         System.out.println("master test");
         System.out.println("hot-fix! test");
+        System.out.println("push test");
     }
 }
